54c3f87fa69419852d724267d7975583600889a9
Trina Lu (陸頤珊) <TrinaL@interserv.com.tw>
2017-01-19 16:04:58 +0800

    initial commit

---
+++ split.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const raw = fs.readFileSync("./raw.txt", "utf8");
+
+const step1 = (raw) => {
+    return raw.trim().replace(/\r/g,"")
+        .split("\n")
+        .map(row => {
+            return row.split(" ")
+                .reduce((sum, current, currentIndex, array) => {
+                    if (currentIndex !== 0) {
+                        const prev = parseInt(array[currentIndex-1]);
+                        sum.push([prev, parseInt(current)]);
+                    }
+                    return sum;
+                }, []);
+        });
+};
+
+const step2 = (step1) => (key) => {
+    let a = [];
+    let b = [];
+    step1.forEach(row => {
+        let startIndex = row.length;
+        row.forEach((group, index) => {
+            const [first, second] = group;
+            if (first === key) {
+                startIndex = index;
+                return false;
+            }
+        });
+        const del = row.length - startIndex;
+        b.push(...row.splice(startIndex, del));
+        a.push(...row);
+    });
+    return {a, b};
+};
+
+const step3 = (step2) => {
+    const {a, b} = step2;
+    return b.filter(groupB => {
+        return !a.some(groupA => {
+            const [a0, a1] = groupA;
+            const [b0, b1] = groupB;
+            return (a0 === b0 && a1 === b1);
+        });
+    });
+};
+
+const result = step3(step2(step1(raw))(3))
+    .map(group => `(${group.toString()})`)
+    .join(' ');
+    
+console.log(result);
